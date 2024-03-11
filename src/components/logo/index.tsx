3fa76d6c068b66778577990e63c73f5ee3b68d98
InnaShp <107990414+InnaShp@users.noreply.github.com>
2024-03-11 17:18:20 +0200

    Update logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -2,10 +2,17 @@ import logo from "../../assets/logo/logo.svg";
 import RouterLink from "../../routes/routerLink";
 import { paths } from "../../config/path";
 
-const Logo = () => {
+interface LogoProps {
+  width: string;
+  height: string;
+}
+const Logo = ({width, height} : LogoProps) => {
   return (
     <RouterLink to={paths.HOME}>
-      <img src={logo} alt="Logo" />
+      <img src={logo} alt="Logo" style={{
+        width: width,
+        height: height
+      }}/>
     </RouterLink>
   );
 };
