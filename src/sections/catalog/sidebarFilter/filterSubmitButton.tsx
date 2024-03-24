30a90e4401ee78d891cc6b6bf6e5f3d63f720eef
InnaShp <107990414+InnaShp@users.noreply.github.com>
2024-03-24 16:59:39 +0200

    Received the queryString value with filters

--- src/sections/catalog/sidebarFilter/filterSubmitButton.tsx
+++ src/sections/catalog/sidebarFilter/filterSubmitButton.tsx
@@ -1,20 +1,26 @@
 import { Button, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 
-
-
-function FilterSubmitButton() {
+function FilterSubmitButton({ onClick }: { onClick: () => void }) {
   const theme = useTheme();
 
   return (
-    <Button sx={{
-      backgroundColor: theme.palette.info.dark,
-      color: theme.palette.secondary.light,
-      width: "140px",
-      height: "44px", 
-      borderRadius: "4px",
-    }}>
-      <Typography variant="button" sx={{fontSize: "16px", lineHeight: "24px"}}>Submit</Typography>
+    <Button
+      sx={{
+        backgroundColor: theme.palette.info.dark,
+        color: theme.palette.secondary.light,
+        width: "140px",
+        height: "44px",
+        borderRadius: "4px",
+      }}
+      onClick={onClick}
+    >
+      <Typography
+        variant="button"
+        sx={{ fontSize: "16px", lineHeight: "24px" }}
+      >
+        Submit
+      </Typography>
     </Button>
   );
 }
--- src/sections/catalog/sidebarFilter/index.tsx
+++ src/sections/catalog/sidebarFilter/index.tsx
@@ -1,6 +1,6 @@
+import { useState, useMemo, useEffect } from "react";
 import { Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-
 import FilterHeader from "./filterHeader";
 import PriceRange from "./priceRange";
 import WineCheckbox from "./wineCheckbox";
@@ -9,6 +9,72 @@ import FilterSubmitButton from "./filterSubmitButton";
 
 function SidebarFilter() {
   const theme = useTheme();
+  const [selectedFilters, setSelectedFilters] = useState<{
+    [key: string]: string[];
+  }>({});
+  const [resetFilters, setResetFilters] = useState(false); 
+  const [queryString, setQueryString] = useState("");
+
+  const constructQueryString = (filters: { [key: string]: string[] }): string => {
+    let queryString = Object.entries(filters)
+      .filter(([_, value]) => value.length > 0)
+      .map(([key, value]) => {
+        const updatedKey = key.replace(/Wine/gi, "").trim();
+        const updatedValues = value.map((val) =>
+          val.replace(/Wine/gi, "").trim()
+        );
+        return `${updatedKey.toLowerCase()}=${updatedValues.join("&")}`;
+      })
+      .join("&");
+
+    if (queryString.startsWith("&")) {
+      queryString = queryString.slice(1);
+    }
+    return queryString;
+  }
+
+  const constructedQueryString = useMemo(() => constructQueryString(selectedFilters), [
+    selectedFilters,
+  ]);
+
+  useEffect(() => {
+    if (queryString) {
+      console.log(queryString);
+    }
+  }, [queryString]);
+
+  const handleFilterChange = (
+    filterTitle: string,
+    filterItem: string,
+    isChecked: boolean
+  ) => {
+    setSelectedFilters((prevFilters) => {
+      if (isChecked) {
+        return {
+          ...prevFilters,
+          [filterTitle]: [...(prevFilters[filterTitle] || []), filterItem],
+        };
+      } else {
+        const updatedFilters = { ...prevFilters };
+        updatedFilters[filterTitle] = updatedFilters[filterTitle].filter(
+          (item) => item !== filterItem
+        );
+        return updatedFilters;
+      }
+    });
+  };
+
+  const handleFilterSubmit = () => {
+    setQueryString(constructedQueryString);
+    setResetFilters(true); // Встановлення значення для очищення фільтрів
+  };
+
+  useEffect(() => {
+    if (resetFilters) {
+      setSelectedFilters({}); // Очищення фільтрів після подання
+      setResetFilters(false); // Скидання значення для очищення фільтрів
+    }
+  }, [resetFilters]);
 
   return (
     <Box
@@ -19,12 +85,27 @@ function SidebarFilter() {
         gap: "40px",
       }}
     >
-      <FilterHeader />
+      <FilterHeader onClick={() => setResetFilters(true)} />
       <PriceRange />
-      <WineCheckbox title="Wine color" items={wineColor} />
-      <WineCheckbox title="Wine type" items={wineType} />
-      <WineCheckbox title="Wine country" items={wineCountry} />
-      <FilterSubmitButton />
+      <WineCheckbox
+        title="Wine color"
+        items={wineColor}
+        onFilterChange={handleFilterChange}
+        resetFilters={resetFilters}
+      />
+      <WineCheckbox
+        title="Wine type"
+        items={wineType}
+        onFilterChange={handleFilterChange}
+        resetFilters={resetFilters} 
+      />
+      <WineCheckbox
+        title="Wine country"
+        items={wineCountry}
+        onFilterChange={handleFilterChange}
+        resetFilters={resetFilters}
+      />
+      <FilterSubmitButton onClick={handleFilterSubmit} />
     </Box>
   );
 }
