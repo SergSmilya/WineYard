111ad072233e600d5608fa325ba51cc5f71eba36
InnaShp <107990414+InnaShp@users.noreply.github.com>
2024-04-09 15:04:35 +0300

    Update WineTour section

--- src/sections/about/aboutHero.tsx
+++ src/sections/about/aboutHero.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 
@@ -6,41 +7,60 @@ import background from "../../assets/about/about-bg.jpeg";
 
 function AboutHero() {
   const theme = useTheme();
+  const [loaded, setLoaded] = useState(false);
+
+  const handleLoad = () => {
+    setLoaded(true);
+  };
+
   return (
-    <Box
-      sx={{
-        backgroundImage: `url(${background})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: "810px",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "31px",
-        boxShadow: "inset 0 0 0 2000px #00000080",
-      }}
-    >
-      <Stack>
-        <img src={logo} alt="Wineyard" />
-      </Stack>
-      <Stack
-        sx={{
-          maxWidth: "520px",
-          textAlign: "center",
-          color: theme.palette.info.main,
-        }}
-      >
-        <Typography variant="h3" sx={{ lineHeight: "25px", marginBottom: "20px" }}>
-          Find exceptional wines from across Europe, from Italy's sun-kissed
-          vineyards to France's vibrant cellars.
-        </Typography>
-        <Typography variant="h3" sx={{ lineHeight: "25px" }}>
-          We deliver to your door, offer alcohol-free choices, and help you pair
-          your wine with every dish.
-        </Typography>
-      </Stack>
-    </Box>
+    <>
+      <img
+        src={background}
+        alt=""
+        onLoad={handleLoad}
+        style={{ display: "none" }}
+      />
+      {loaded ? (
+        <Box
+          sx={{
+            backgroundImage: `url(${background})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "810px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "81px",
+            boxShadow: `${loaded ? "inset 0 0 0 2000px #00000080" : ""}`,
+          }}
+        >
+          <Stack>
+            <img src={logo} loading="lazy" alt="Wineyard" />
+          </Stack>
+          <Stack
+            sx={{
+              maxWidth: "520px",
+              textAlign: "center",
+              color: theme.palette.info.main,
+            }}
+          >
+            <Typography
+              variant="h3"
+              sx={{ lineHeight: "25px", marginBottom: "30px" }}
+            >
+              Find exceptional wines from across Europe, from Italy's sun-kissed
+              vineyards to France's vibrant cellars.
+            </Typography>
+            <Typography variant="h3" sx={{ lineHeight: "25px" }}>
+              We deliver to your door, offer alcohol-free choices, and help you
+              pair your wine with every dish.
+            </Typography>
+          </Stack>
+        </Box>
+      ) : null}
+    </>
   );
 }
 
--- src/sections/about/wineTour.tsx
+++ src/sections/about/wineTour.tsx
@@ -1,9 +1,87 @@
-function WineTour() {
-  return (
-    <div>
-      WineTour
-    </div>
-  )
+import { Box, Stack, Typography } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
+import wineStand from "../../assets/about/wine-stand.jpg";
+
+interface ListItemProp {
+  count: string;
+  text: string;
 }
 
-export default WineTour
+function WineTour() {
+  const theme = useTheme();
+
+  const colors = {
+    red: theme.palette.primary.main,
+    green: theme.palette.secondary.light,
+    dark: theme.palette.success.main,
+  };
+
+  const ListItem = ({ count, text }: ListItemProp) => (
+    <Box sx={{ width: "264px" }}>
+      <Typography
+        variant="h2"
+        sx={{
+          fontSize: "48px",
+          lineHeight: "72px",
+          color: colors.red,
+          marginBottom: "5px",
+        }}
+      >
+        {count}
+      </Typography>
+      <Typography
+        variant="h5"
+        sx={{ fontSize: "22px", lineHeight: "28px", color: colors.dark }}
+      >
+        {text}
+      </Typography>
+    </Box>
+  );
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "96px",
+        marginTop: "60px",
+      }}
+    >
+      <Stack sx={{ maxWidth: "576px", maxHeight: "662px" }}>
+        <img src={wineStand} alt="Wine stand" />
+      </Stack>
+      <Stack sx={{ maxWidth: "576px" }}>
+        <Typography
+          variant="h5"
+          sx={{ color: colors.red, marginBottom: "5px" }}
+        >
+          We invite you to explore the world of wine with us
+        </Typography>
+        <Typography
+          variant="h4"
+          sx={{ lineHeight: "60px", color: colors.green, marginBottom: "50px" }}
+        >
+          Bringing Europe's Wines to Your Doorstep
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            columnGap: "32px",
+            rowGap: "48px",
+          }}
+        >
+          <ListItem count="5K+" text="happy returning customers" />
+          <ListItem count="9K+" text="monthly visitors" />
+          <ListItem count="48" text="countries presence (the whole Europe)" />
+          <ListItem count="55" text="warehouses throughout Europe" />
+        </Box>
+      </Stack>
+    </Box>
+  );
+}
+
+export default WineTour;
