7740bd6ba8a504d9d183f10a51d158b80dbf93d8
Smilichenko <110824059+SergSmilya@users.noreply.github.com>
2024-04-18 20:04:36 +0300

    added page collection almost finished

--- src/pages/collections.tsx
+++ src/pages/collections.tsx
@@ -1,7 +1,17 @@
-function Collections() {
+import { Box, Container } from "@mui/material";
+import BenefitsSec from "../sections/main/BenefitsSec";
+import HowItWorkSection from "../sections/collection/HowItWorkSection";
+import HeroSection from "../sections/collection/HeroSection";
+
+
+export default function Collections() {
   return (
-    <div>Hello from Collections</div>
+    <Box>
+      <Container>
+        <HeroSection />
+        <BenefitsSec />
+        <HowItWorkSection />
+      </Container>
+    </Box>
   )
 }
-
-export default Collections
