2bec55dfea1977c99f21a248773956ed91cf1e4f
Smilichenko <110824059+SergSmilya@users.noreply.github.com>
2024-06-04 13:58:13 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,8 @@ import { HashRouter } from "react-router-dom";
 // const GOOGLE_API_TOKEN = process.env.REACT_APP_WineYard_GOOGLE_API_TOKEN;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_API_TOKEN}>
+  <GoogleOAuthProvider clientId='985644577186-sr0tejl4c42qncivrlcqcls1e28rqv6p.apps.googleusercontent.com'>
+  {/* <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_API_TOKEN}> */}
     <React.StrictMode>
       <HashRouter basename="/">
         <Provider store={store}>
