9978fdb7b107bb56ed5fc78aca68ccac9e7ef2c6
InnaShp <107990414+InnaShp@users.noreply.github.com>
2024-06-05 13:45:52 +0300

    Update panelFilterDishMobile

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -12,13 +12,19 @@ import "./slider.css";
 interface SliderProps {
   children: ReactNode;
   handleSlideChange?: (swiper: any) => void;
+  initialSlide?: number;
+  onSwiper?: (swiper: any) => void;
 }
 
-function Slider({ children, handleSlideChange }: SliderProps) {
-
+function Slider({
+  children,
+  handleSlideChange,
+  initialSlide,
+  onSwiper,
+}: SliderProps) {
   return (
     <Swiper
-      loop= {true}
+      loop={true}
       slidesPerView={1}
       keyboard={{
         enabled: true,
@@ -28,6 +34,8 @@ function Slider({ children, handleSlideChange }: SliderProps) {
       }}
       navigation
       onSlideChange={handleSlideChange}
+      initialSlide={initialSlide}
+      onSwiper={onSwiper}
       modules={[Keyboard, Pagination, Navigation]}
     >
       {React.Children.map(children, (child) => (
--- src/sections/main/SecWineDish/panelFilterDishMobile.tsx
+++ src/sections/main/SecWineDish/panelFilterDishMobile.tsx
@@ -1,8 +1,8 @@
-import { useDispatch } from "react-redux";
+import { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { setActiveCategory } from "../../../store/categoriesSlice";
 import Slider from "../../../components/Slider";
 import { Typography } from "@mui/material";
-
 import "./index.css";
 import { success } from "../../../theme/palette";
 
@@ -16,16 +16,39 @@ interface Category {
 }
 
 export default function PanelFilterDishMobile({ categories }: PanelFilterDish) {
+  const activeCategory = useSelector(
+    (state: any) => state.categories.activeCategory
+  );
   const dispatch = useDispatch();
+  const swiperRef = useRef<any>(null);
 
   const handleSlideChange = (swiper: any) => {
     const categoryId = categories[swiper.realIndex].id;
     dispatch(setActiveCategory(categoryId));
   };
 
+  const initialSlide = categories.findIndex(
+    (category) => category.id === activeCategory
+  );
+
+  useEffect(() => {
+    if (swiperRef.current) {
+      const categoryIndex = categories.findIndex(
+        (category) => category.id === activeCategory
+      );
+      if (categoryIndex !== -1) {
+        swiperRef.current.slideToLoop(categoryIndex);
+      }
+    }
+  }, [activeCategory, categories]);
+
   return (
-    <div className="dishPanel">
-      <Slider handleSlideChange={handleSlideChange}>
+    <div className="dishPanel" id="wine-with-dish">
+      <Slider
+        handleSlideChange={handleSlideChange}
+        initialSlide={initialSlide}
+        onSwiper={(swiper: any) => (swiperRef.current = swiper)}
+      >
         {categories.map(({ id, name }) => (
           <Typography
             key={id}
