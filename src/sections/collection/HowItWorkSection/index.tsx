dcc5b16f74867e9eba77a843826258e30dcbfb5c
InnaShp <107990414+InnaShp@users.noreply.github.com>
2024-07-20 19:27:59 +0300

    Remove unnecessary tags

--- src/sections/collection/HowItWorkSection/index.tsx
+++ src/sections/collection/HowItWorkSection/index.tsx
@@ -5,84 +5,83 @@ import { primary } from "../../../theme/palette";
 
 export default function HowItWorkSection() {
   return (
-    <Box sx={{ padding: {lg: "60px 0 100px"} }}>
-      <Stack
+    <Box
+      sx={{
+        padding: { lg: "60px 0 100px" },
+        display: "flex",
+        flexDirection: { xs: "column-reverse", lg: "row" },
+        alignItems: "center",
+        gap: { xs: "30px", lg: "96px" },
+      }}
+    >
+      <Box
         sx={{
+          overflow: "hidden",
           display: "flex",
-          flexDirection: { xs: "column-reverse", lg: "row" },
           alignItems: "center",
-          gap: {xs: "30px", lg: "96px"},
+          justifyContent: "center",
+          maxHeight: "517px",
+          maxWidth: "576px",
         }}
       >
-        <Box
-          sx={{
-            overflow: "hidden",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            maxHeight: "517px",
-            maxWidth: "576px",
-          }}
-        >
-          <img src={ImgHowItWork} alt="desc" loading="lazy" />
+        <img src={ImgHowItWork} alt="desc" loading="lazy" />
+      </Box>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        gap={{ xs: "15px", lg: "50px" }}
+        maxWidth={"500px"}
+      >
+        <TitleComp size="109%" position="left">
+          How it works
+        </TitleComp>
+        <Box display={"flex"} flexDirection={"column"} gap={"30px"}>
+          <Stack display={"flex"} direction={"column"} gap={"15px"}>
+            <Typography
+              sx={{
+                color: primary.main,
+                fontSize: { xs: "24px", lg: "36px" },
+                lineHeight: { xs: "28px", lg: "36px" },
+                fontWeight: 600,
+              }}
+            >
+              1. Pick Your Wine Passport
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: { xs: "16px", lg: "18px" },
+                lineHeight: { xs: "20px", lg: "28px" },
+              }}
+            >
+              Imagine yourself strolling through Italian vineyards, exploring
+              charming French villages, or feeling the cool air of a Georgian
+              cellar. Choose the country that sparks your curiosity!
+            </Typography>
+          </Stack>
+          <Stack display={"flex"} direction={"column"} gap={"15px"}>
+            <Typography
+              sx={{
+                color: primary.main,
+                fontSize: { xs: "24px", lg: "36px" },
+                lineHeight: { xs: "28px", lg: "36px" },
+                fontWeight: 600,
+              }}
+            >
+              2. Order & Embark on Your Journey!
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: { xs: "16px", lg: "18px" },
+                lineHeight: { xs: "20px", lg: "28px" },
+              }}
+            >
+              Place your order and get ready to embark on a delicious adventure!
+              Each box is packed with carefully chosen wines, ready to transport
+              your taste buds to a new corner of the world.
+            </Typography>
+          </Stack>
         </Box>
-        <Box
-          display={"flex"}
-          flexDirection={"column"}
-          gap={{ xs: "15px", lg: "50px" }}
-          maxWidth={"500px"}
-        >
-          <TitleComp size="109%" position="left">
-            How it works
-          </TitleComp>
-          <Box display={"flex"} flexDirection={"column"} gap={"30px"}>
-            <Stack display={"flex"} direction={"column"} gap={"15px"}>
-              <Typography
-                sx={{
-                  color: primary.main,
-                  fontSize: { xs: "24px", lg: "36px" },
-                  lineHeight: { xs: "28px", lg: "36px" },
-                  fontWeight: 600,
-                }}
-              >
-                1. Pick Your Wine Passport
-              </Typography>
-              <Typography
-                sx={{
-                  fontSize: { xs: "16px", lg: "18px" },
-                  lineHeight: { xs: "20px", lg: "28px" },
-                }}
-              >
-                Imagine yourself strolling through Italian vineyards, exploring
-                charming French villages, or feeling the cool air of a Georgian
-                cellar. Choose the country that sparks your curiosity!
-              </Typography>
-            </Stack>
-            <Stack display={"flex"} direction={"column"} gap={"15px"}>
-              <Typography
-                sx={{
-                  color: primary.main,
-                  fontSize: { xs: "24px", lg: "36px" },
-                  lineHeight: { xs: "28px", lg: "36px" },
-                  fontWeight: 600,
-                }}
-              >
-                2. Order & Embark on Your Journey!
-              </Typography>
-              <Typography
-                sx={{
-                  fontSize: { xs: "16px", lg: "18px" },
-                  lineHeight: { xs: "20px", lg: "28px" },
-                }}
-              >
-                Place your order and get ready to embark on a delicious
-                adventure! Each box is packed with carefully chosen wines, ready
-                to transport your taste buds to a new corner of the world.
-              </Typography>
-            </Stack>
-          </Box>
-        </Box>
-      </Stack>
+      </Box>
     </Box>
   );
 }
--- src/sections/giftBox/WhatInsideSection/index.tsx
+++ src/sections/giftBox/WhatInsideSection/index.tsx
@@ -33,65 +33,63 @@ const BoxDescriptionStyle = {
 
 export default function WhatInsideSection() {
   return (
-    <Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: { xs: "column", lg: "row" },
-          gap: { xs: "24px" },
-          justifyContent: "space-between",
-        }}
-      >
-        {/* Description */}
-        <Box sx={BoxDescriptionStyle}>
-          <TitleComp size="109%" position="left">
-            What’s inside?
-          </TitleComp>
-          <List>
-            <ListItem>
-              <Typography sx={commonTextStyle}>
-                Each box contains 6 bottles
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <Typography sx={commonTextStyle}>
-                The wines in the box are all wrapped
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <Typography sx={commonTextStyle}>
-                Each bottle contains a unique code and card with all information
-                about the wine and winemaker
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <Typography sx={commonTextStyle}>
-                Delivered to your home in a nice sturdy box
-              </Typography>
-            </ListItem>
-            <ListItem>
-              <Typography sx={commonTextStyle}>
-                Made with the expertise of sommeliers
-              </Typography>
-            </ListItem>
-          </List>
-          <Typography
-            sx={{
-              fontSize: { xs: "16px", lg: "18px" },
-              lineHeight: { xs: "20px", lg: "28px" },
-              fontWeight: 600,
-            }}
-          >
-            Enjoy your dose of grapes and friends!
-          </Typography>
-        </Box>
-        <Box>
-          <img
-            srcSet={`${imgBoutles} 1x, ${imgBoutles2X} 2x`}
-            src={imgBoutles}
-            alt="six-boutles"
-          />
-        </Box>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", lg: "row" },
+        gap: { xs: "24px" },
+        justifyContent: "space-between",
+      }}
+    >
+      {/* Description */}
+      <Box sx={BoxDescriptionStyle}>
+        <TitleComp size="109%" position="left">
+          What’s inside?
+        </TitleComp>
+        <List>
+          <ListItem>
+            <Typography sx={commonTextStyle}>
+              Each box contains 6 bottles
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <Typography sx={commonTextStyle}>
+              The wines in the box are all wrapped
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <Typography sx={commonTextStyle}>
+              Each bottle contains a unique code and card with all information
+              about the wine and winemaker
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <Typography sx={commonTextStyle}>
+              Delivered to your home in a nice sturdy box
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <Typography sx={commonTextStyle}>
+              Made with the expertise of sommeliers
+            </Typography>
+          </ListItem>
+        </List>
+        <Typography
+          sx={{
+            fontSize: { xs: "16px", lg: "18px" },
+            lineHeight: { xs: "20px", lg: "28px" },
+            fontWeight: 600,
+          }}
+        >
+          Enjoy your dose of grapes and friends!
+        </Typography>
+      </Box>
+      <Box>
+        <img
+          srcSet={`${imgBoutles} 1x, ${imgBoutles2X} 2x`}
+          src={imgBoutles}
+          alt="six-boutles"
+        />
       </Box>
     </Box>
   );
